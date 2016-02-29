f2d5aa15a0a4710fee7072f8dad4fee249228e1b
duktech <marius.rata@duk-tech.com>
2016-02-29 10:52:40 +0200

    more

--- js/index.js
+++ js/index.js
@@ -20,44 +20,7 @@ var app = {
     // Application Constructor
     initialize: function() {
         this.bindEvents();
-        
-        alert("start creating the DB");
-			var myDB = window.sqlitePlugin.openDatabase({name: "mySQLite.db"});
-			
-			myDB.transaction(function(transaction) {
-transaction.executeSql('CREATE TABLE IF NOT EXISTS phonegap_pro (id integer primary key, title text, desc text)', [],
-function(tx, result) {
-alert("Table created successfully");
-},
-function(error) {
-alert("Error occurred while creating the table.");
-});
-});
-
-var title="sundaravel";
-var desc="phonegap freelancer";
-myDB.transaction(function(transaction) {
-var executeQuery = "INSERT INTO phonegap_pro (title, desc) VALUES (?,?)";
-transaction.executeSql(executeQuery, [title,desc]
-, function(tx, result) {
-alert('Inserted');
-},
-function(error){
-alert('Error occurred');
-});
-});
-
-myDB.transaction(function(transaction) {
-transaction.executeSql('SELECT * FROM phonegap_pro', [], function (tx, results) {
-var len = results.rows.length, i;
-$("#rowCount").append(len);
-for (i = 0; i < len; i++){
-$("#TableData").append("<tr><td>"+results.rows.item(i).id+"</td><td>"+results.rows.item(i).title+"</td><td>"+results.rows.item(i).desc+"</td></tr>");
-}
-}, null);
-});
-        
-        
+		alert("Hello GUYS");
     },
     // Bind Event Listeners
     //
