7e0f5388f8664f8378a5a2cf4e211d87df5a191f
mugurelrata <mugurel_rata@yahoo.com>
2016-03-02 13:12:41 +0200

    change Team DB

--- js/db.js
+++ js/db.js
@@ -122,14 +122,14 @@ var dbWrapper = {
 	
 	getPatientByInterventionId: function(id, getPatientByInterventionIdReturnFunction){
 		db.transaction(function(transaction) {
-			transaction.executeSql('SELECT * FROM Pacient WHERE InterventionId = '+ id +' ORDER BY Id DESC;', [],
+			transaction.executeSql('SELECT * FROM Team WHERE InterventionId = '+ id +' ORDER BY Id DESC;', [],
 			getPatientByInterventionIdReturnFunction ,this.errorHandler);
 		},this.errorHandler,this.nullHandler);
 	},
 	
 	addTeam: function(interventionId, name, surname, role, isLeader,addTeamReturnFunction){
 		db.transaction(function(transaction) {
-			transaction.executeSql('INSERT INTO Pacient(InterventionId, Name, Surname, Role, IsLeader) VALUES (?,?,?,?,?)',[interventionId, name, surname, role,isLeader], addTeamReturnFunction, this.errorHandler);
+			transaction.executeSql('INSERT INTO Team(InterventionId, Name, Surname, Role, IsLeader) VALUES (?,?,?,?,?)',[interventionId, name, surname, role, isLeader], addTeamReturnFunction, this.errorHandler);
 		});
 
 		return false;
