037e98bfc5d479b00a380136ec00ae77d11de1bc
Christian Oak <christianoakleaf@gmail.com>
2016-03-14 17:40:58 +0200

    update af-c

--- js/index.js
+++ js/index.js
@@ -47,7 +47,7 @@ function getLastInterventionReturnFunction(transaction, result) {
 function getPatientByInterventionIdReturnFunction(transaction, result) {
 	if (result != null && result.rows != null && result.rows.length > 0) {
 		var row = result.rows.item(0);
-		$('#patientDetails').append('<br/> Name: ' + row.Name + ' <br /> Surname: ' + row.Surname + ' <br /> BirthDate: ' + row.BirthDate.substr(0, 4) + " / " + row.BirthDate.substr(4, 2) + " / " + row.BirthDate.substr(6, 2) + "<br /> Hospital No: " + row.HospitalNr);
+		$('#patientDetails').append('<span class="green-title"> ' + row.Name + ' ' + row.Surname + ' </span>' + ' <br /> DOB: ' + row.BirthDate.substr(0, 4) + " / " + row.BirthDate.substr(4, 2) + " / " + row.BirthDate.substr(6, 2) + "<br /> Hospital No: " + row.HospitalNr);
 	}
 }
 
