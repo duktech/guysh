6484eb0b48cb8b593326559465737d40523015f9
Flondor Stefan <flondorel@gmail.com>
2016-03-18 17:23:55 +0200

    only design update

--- js/index.js
+++ js/index.js
@@ -27,6 +27,7 @@ function getAllInterventionsReturnFunction(transaction, result) {
 }
 
 function getLastInterventionReturnFunction(transaction, result) {
+	console.log(result);
 	if (result != null && result.rows != null && result.rows.length > 0) {
 		var row = result.rows.item(0);
 		window.localStorage.setItem("lastIntervention", row.Id);
@@ -47,7 +48,7 @@ function getLastInterventionReturnFunction(transaction, result) {
 function getPatientByInterventionIdReturnFunction(transaction, result) {
 	if (result != null && result.rows != null && result.rows.length > 0) {
 		var row = result.rows.item(0);
-		$('#patientDetails').append('<span class="green-title"> ' + row.Name + ' ' + row.Surname + ' </span>' + '<table class="patienttable">' + '<tr>' + '<td class="lcol">' + 'DOB:' + '</td>' + '<td class="lcol">' + row.BirthDate.substr(0, 4) + " / " + row.BirthDate.substr(4, 2) + " / " + row.BirthDate.substr(6, 2) + '</td>' + '</tr>' + '<tr>' + '<td class="lcol">' + "Hospital No:" + '</td>' + '<td class="lcol">' + row.HospitalNr + '</td>' + '</tr>' + '</table>');
+		$('#patientDetails').prepend('<span class="green-title"> ' + row.Name + ' ' + row.Surname + ' </span>' + '<table class="patienttable">' + '<tr>' + '<td class="lcol">' + 'DOB:' + '</td>' + '<td class="lcol">' + row.BirthDate.substr(0, 4) + " / " + row.BirthDate.substr(4, 2) + " / " + row.BirthDate.substr(6, 2) + '</td>' + '</tr>' + '<tr>' + '<td class="lcol">' + "Hospital No:" + '</td>' + '<td class="lcol">' + row.HospitalNr + '</td>' + '</tr>' + '</table>');
 	}
 }
 
